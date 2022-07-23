883ebd8fc9b4901baf02c2f06655da304b36b05d
Raul Fulguera Colque <raul.e.fulguera@gmail.com>
2022-07-23 08:12:24 -0400

    cambio en format

--- Reservas.Domain/Model/Reservas/Reserva.cs
+++ Reservas.Domain/Model/Reservas/Reserva.cs
@@ -22,57 +22,57 @@ namespace Reservas.Domain.Model.Reservas {
 		private readonly ICollection<VueloReserva> _vueloReserva;
 
 
-		public IReadOnlyCollection<VueloReserva> VueloReserva
+		public IReadOnlyCollection<VueloReserva> VueloReserva {
 			get {
 				return new ReadOnlyCollection<VueloReserva>(_vueloReserva.ToList());
 			}
-}
-private Reserva() {
-	public Reserva(Guid idVuelo, string nroReserva, DateTime fechaVuelo) {
-		DateTime horaActual = DateTime.Now;
-		DateTime horaLimite = horaActual.AddHours(2);
-
-
-		Id = Guid.NewGuid();
-		IdVuelo = idVuelo;
-		NroReserva = nroReserva;
-		Costo = new PrecioValue(0m);
-		Fecha = DateTime.Now;
-		FechaVuelo = fechaVuelo;
-		Hora = TimeSpan.Parse(horaActual.ToString("HH:mm"));
-		HoraLimite = TimeSpan.Parse(horaLimite.ToString("HH:mm"));
-		Estado = 'R';
-		Activo = true;
-		_vueloReserva = new List<VueloReserva>();
-	}
-
-	public void AgregarItem(Guid idPasajero, decimal costo) {
-		var detalleReserva = _vueloReserva.FirstOrDefault(x => x.IdPasajero == idPasajero);
-		if (detalleReserva is null)
-		{
-			detalleReserva = new VueloReserva(idPasajero, costo);
-			_vueloReserva.Add(detalleReserva);
 		}
-		else
-		{
-			detalleReserva.ModificarReserva(idPasajero, costo);
+		private Reserva() {
+			public Reserva(Guid idVuelo, string nroReserva, DateTime fechaVuelo) {
+				DateTime horaActual = DateTime.Now;
+				DateTime horaLimite = horaActual.AddHours(2);
+
+
+				Id = Guid.NewGuid();
+				IdVuelo = idVuelo;
+				NroReserva = nroReserva;
+				Costo = new PrecioValue(0m);
+				Fecha = DateTime.Now;
+				FechaVuelo = fechaVuelo;
+				Hora = TimeSpan.Parse(horaActual.ToString("HH:mm"));
+				HoraLimite = TimeSpan.Parse(horaLimite.ToString("HH:mm"));
+				Estado = 'R';
+				Activo = true;
+				_vueloReserva = new List<VueloReserva>();
+			}
+
+			public void AgregarItem(Guid idPasajero, decimal costo) {
+				var detalleReserva = _vueloReserva.FirstOrDefault(x => x.IdPasajero == idPasajero);
+				if (detalleReserva is null)
+				{
+					detalleReserva = new VueloReserva(idPasajero, costo);
+					_vueloReserva.Add(detalleReserva);
+				}
+				else
+				{
+					detalleReserva.ModificarReserva(idPasajero, costo);
+				}
+				Costo += costo;
+				AddDomainEvent(new ItemReservaAgregado(idPasajero, Costo));
+			}
+
+			public void AnularReserva() {
+				Estado = 'A';
+				Activo = false;
+			}
+
+			public void VentaReserva() {
+				Estado = 'V';
+			}
+
+			public void ConsolidarReserva() {
+				var evento = new ReservaCreado(Id, NroReserva, Costo, IdVuelo);
+				AddDomainEvent(evento);
+			}
 		}
-		Costo += costo;
-		AddDomainEvent(new ItemReservaAgregado(idPasajero, Costo));
-	}
-
-	public void AnularReserva() {
-		Estado = 'A';
-		Activo = false;
-	}
-
-	public void VentaReserva() {
-		Estado = 'V';
-	}
-
-	public void ConsolidarReserva() {
-		var evento = new ReservaCreado(Id, NroReserva, Costo, IdVuelo);
-		AddDomainEvent(evento);
-	}
-}
 	}
