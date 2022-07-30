87197ffcae8e22170b1a0332e848d2ee8b680c04
Raul Fulguera Colque <raul.e.fulguera@gmail.com>
2022-07-30 07:13:46 -0400

    test

--- Reservas.WebApi/Startup.cs
+++ Reservas.WebApi/Startup.cs
@@ -28,11 +28,10 @@ namespace Reservas.WebApi {
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if (env.IsDevelopment())
 			{
-				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reservas.WebApi v1"));
+				app.UseDeveloperExceptionPage(); s
 			}
-
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reservas.WebApi v1"));
 			app.UseRouting();
 
 			app.UseAuthorization();
